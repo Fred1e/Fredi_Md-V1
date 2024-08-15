a166435605300407432f81faf639391ac87a6356
FredieTech <frediezra60@gmail.com>
2024-08-15 03:21:45 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ global.audio= "" ;
 global.video= "" ;
 global.port =process.env.PORT
 global.appUrl=process.env.APP_URL || ""                       // put your app url here,
-global.email ="ngoyaibraah@gmail.com"
+global.email ="frediezra60@gmail.com"
 global.location="Nairobi,Kenya."
 
 
@@ -17,16 +17,16 @@ global.blockJids= process.env.BLOCK_JID || "null"
 global.DATABASE_URL = process.env.DATABASE_URL || ""
 
 global.timezone= process.env.TZ || process.env.TIME_ZONE || "Africa/Kenya";
-global.github=process.env.GITHUB|| "https://github.com/ibrahimaitech/BLACK-PANTHER-MD/tree/main";
-global.gurl  =process.env.GURL  || "https://whatsapp.com/channel/0029VaZuGSxEawdxZK9CzM0Y";
-global.website=process.env.GURL || "https://chat.whatsapp.com/GoPZhE9zc6Y3ul0RnD4HXw" ; 
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/e5834a2b98545c5c963d2.jpg" ; // SET LOGO FOR IMAGE 
+global.github=process.env.GITHUB|| "https://github.com/Fred1e/FREDI_MD-plugin/tree/main";
+global.gurl  =process.env.GURL  || "https://whatsapp.com/channel/0029VaihcQv84Om8LP59fO3f";
+global.website=process.env.GURL || "https://whatsapp.com/channel/0029VaihcQv84Om8LP59fO3f" ; 
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/a8b74780a8d18240b1d82.jpg" ; // SET LOGO FOR IMAGE 
 
 
 
-global.devs = "254710772666,255620814108, 255766352895" // Developer Contact
+global.devs = "255752593977,255620814108,255766352895" // Developer Contact
 global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "null";
-global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "254710772666,255620814108, 255766352895";
+global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "255752593977,255620814108, 255766352895";
 
 
 
@@ -42,24 +42,24 @@ global.disablepm = process.env.DISABLE_PM || "false"
 global.disablegroup = process.env.DISABLE_GROUPS || "false", // disable bot in groups when public mode
 
 global.MsgsInLog = process.env.MSGS_IN_LOG|| "false" // "true"  to see messages , "log" to open logs , "false" to hide logs messages
-global.userImages= process.env.USER_IMAGES || "https://telegra.ph/file/e5834a2b98545c5c963d2.jpg" // BLACKPANTHER Theme Images
+global.userImages= process.env.USER_IMAGES || "https://telegra.ph/file/a8b74780a8d18240b1d82.jpg" // FREDI Theme Images
 global.waPresence= process.env.WAPRESENCE ||  "set according to your need" ; // 'unavailable' | 'available' | 'composing' | 'recording' | 'paused'
 
 
 //========================= [ AUTO READ MSGS & CMDS ] =========================\\
 global.readcmds = process.env.READ_COMMAND || "false"
 global.readmessage = process.env.READ_MESSAGE || "false"
-global.readmessagefrom = process.env.READ_MESSAGE_FROM || "254710772666";
+global.readmessagefrom = process.env.READ_MESSAGE_FROM || "255620814108,255766352895,255752593977";
 
 
 //========================= [ AUTO SAVE & READ STATUS ] =========================\\
 global.read_status = process.env.AUTO_READ_STATUS || "false"
 global.save_status = process.env.AUTO_SAVE_STATUS || "false"
-global.save_status_from =  process.env.SAVE_STATUS_FROM  || "255620814108, 255766352895,254710772666";
-global.read_status_from =  process.env.READ_STATUS_FROM  ||  "254710772666,254710772666255762190568,255679513639";
+global.save_status_from =  process.env.SAVE_STATUS_FROM  || "255620814108,255766352895,255752593977";
+global.read_status_from =  process.env.READ_STATUS_FROM  ||  "255620814108,255766352895,255752593977";
 
 global.api_smd = "https://api-smd-1.vercel.app"
-global.scan = "https://wa.link/4wz6zv";
+global.scan = "https://wa.me/255752593977";
 
 
 global.SESSION_ID = process.env.SESSION_ID ||  "" ;
@@ -72,11 +72,11 @@ module.exports = {
   HANDLERS: process.env.PREFIX  || ".",
   BRANCH  : process.env.BRANCH  || "main",
   VERSION : process.env.VERSION || "1.3.1",
-  caption : process.env.CAPTION || "FREDI_ADAMS_MD✅" , // ```『 ᴘᴏᴡᴇʀᴇᴅ ʙʏ sᴜʜᴀɪʟ²²¹-ᴍᴅ 』```", //*『sᴜʙsᴄʀɪʙᴇ • sᴜʜᴀɪʟ ᴛᴇᴄʜ』*\n youtube.com/@suhailtechinfo0"),
+  caption : process.env.CAPTION || "FREDI_MD✅" , // ```『 ᴘᴏᴡᴇʀᴇᴅ ʙʏ sᴜʜᴀɪʟ²²¹-ᴍᴅ 』```", //*『sᴜʙsᴄʀɪʙᴇ • sᴜʜᴀɪʟ ᴛᴇᴄʜ』*\n youtube.com/@suhailtechinfo0"),
  
   author : process.env.PACK_AUTHER|| "Mr Fredi",
   packname: process.env.PACK_NAME || "🐯",
-  botname : process.env.BOT_NAME  || "FREDI_ADAMS_MD",
+  botname : process.env.BOT_NAME  || "FREDI_MD",
   ownername:process.env.OWNER_NAME|| "Mr Fredi",
 
 
